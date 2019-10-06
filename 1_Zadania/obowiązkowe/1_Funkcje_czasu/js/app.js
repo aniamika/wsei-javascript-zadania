5518991b46f7852430c44ad2c8d478e4bc6679c0
Mika Anna <anna.mika@microsoft.wsei.edu.pl>
2019-10-06 17:01:19 +0200

    Zadanie 1_Funkcje_czasu

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,15 @@
+// Zadanie 1_Funkcje_czasu
+
+function countHello(number) {
+
+    var counter = 1;
+
+    var interval = setInterval(()=>{
+        console.log("Hello", counter++);
+        if (counter > number) {
+            clearInterval(interval);
+        }
+    },500)
+}
+
+countHello(5);
