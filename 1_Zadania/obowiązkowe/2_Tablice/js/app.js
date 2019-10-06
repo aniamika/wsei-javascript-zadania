4ebc2d7c009d69f8c5de8a2fd5b7735df65178fc
Mika Anna <anna.mika@microsoft.wsei.edu.pl>
2019-10-06 17:25:00 +0200

    Zadanie 1_Funkcje_czasu

---
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -0,0 +1,21 @@
+// Zadanie 2_Tablice
+
+function distFromAvarage(array) {
+
+    var averageSum = 0;
+    for (let i = 0; i < array.length; i++) {
+        averageSum += array[i];
+    }
+
+    var averageValue = averageSum / array.length;
+    const outputArray = [];
+    
+    for(let i = 0; i < array.length; i++) {
+        outputArray.push(array[i] - averageValue);
+    };
+
+    console.log(outputArray);
+    return array;
+};
+
+distFromAvarage([1, 2, 3, 4, 5, 6, 7]);
