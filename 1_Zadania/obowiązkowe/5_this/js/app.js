edb7fa86ba236faa182bbead2ef7800a2c2c35d9
Anna Mika <anna.mika@avanade.com>
2020-01-20 11:03:50 +0100

    5_this_0

--- 1_Zadania/obowiązkowe/5_this/js/app.js
+++ 1_Zadania/obowiązkowe/5_this/js/app.js
@@ -1,16 +1,16 @@
 // 5_this_0
 var car = {
     brand: "Mercedes",
-    color: "red",
-    numberOfKilometers: 6
+    color: "Czarny",
+    numberOfKilometers: 6,
 }
 
 car.printCarInfo = function() {
-    console.log(this.color, this.brand, "," , this.numberOfKilometers);
+    console.log(this.color, this.brand, "," , this.numberOfKilometers + "km");
 }
 
 car.drive = function(km) {
-    this.numberOfKilometers = this.numberOfKilometers + km;
+    console.log(this.numberOfKilometers = this.numberOfKilometers + km);
 }
 
 car.printCarInfo();
@@ -18,23 +18,23 @@ car.drive(20);
 car.printCarInfo();
 
 // 5_this_1
-// car.technicalDater = [
-//     '2019-08-19',
-//     '2019-07-19',
-//     '2019-03-19',
-//     '2019-01-19',
-// ];
+car.technicalDates = [
+    '2019-08-19',
+    '2019-07-19',
+    '2019-03-19',
+    '2019-01-19',
+];
 
-// car.addTechnical = function(date) {
-//     this.technicalDates.push(date);
-// }
+car.addTechnical = function(date) {
+    this.technicalDates.push(date);
+}
 
-// car.getTechnicalDates = function() {
-//     console.log(this.technicalDates);
-// };
+car.getTechnicalDates = function() {
+    console.log(this.technicalDates);
+};
 
-// car addTechnical('2020-08-20');
-// car.getTechnicalDates();
+car.addTechnical('2020-08-20');
+car.getTechnicalDates();
 
 
 // 5_this_3
@@ -55,4 +55,4 @@ stairs.up();
 stairs.up();
 stairs.up();
 stairs.down(); 
-stairs.printStep() // 2
+stairs.printStep();
