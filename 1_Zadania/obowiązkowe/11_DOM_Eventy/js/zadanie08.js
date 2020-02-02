16fc4ff7abc35b3a9c716202e5b0aa2dfbb0c9a1
Anna Mika <anna.mika@avanade.com>
2020-02-02 22:24:08 +0100

    11_DOM_Eventy_8

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie08.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie08.js
@@ -1,5 +1,15 @@
 // 11_DOM_Eventy_8
 document.addEventListener('DOMContentLoaded', function(){
 
-    
+    let width = document.getElementById("windowWidth");
+    let height = document.getElementById("windowHeight");
+
+    width.innerText = window.innerWidth;
+    height.innerText = window.innerHeight;
+
+    window.addEventListener("resize", function () {
+        width.innerText = window.innerWidth;
+        height.innerText = window.innerHeight;
+    });
+
 });
