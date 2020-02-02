a550864d400a99c6a4f462bcaedaa3ff968ee9f6
Anna Mika <anna.mika@avanade.com>
2020-02-02 22:15:25 +0100

    11_DOM_Eventy_5

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie05.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie05.js
@@ -1,5 +1,14 @@
 // 11_DOM_Eventy_5
 document.addEventListener('DOMContentLoaded', function(){
 
+    let box = document.querySelectorAll('.box');
+
+    box.forEach(function (element) {
+        element.addEventListener('click', function () {
+            var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+            this.style.backgroundColor = randomColor;
+        })
+    });
+
     
 });
