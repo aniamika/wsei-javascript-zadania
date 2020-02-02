5d7f971e50e6a5e6b0a77be1ac8421806b739a3a
Anna Mika <anna.mika@avanade.com>
2020-02-02 22:18:08 +0100

    11_DOM_Eventy_6

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie06.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie06.js
@@ -1,5 +1,19 @@
 // 11_DOM_Eventy_6
 document.addEventListener('DOMContentLoaded', function(){
 
-    
+    let box = document.querySelector('#box');
+
+    box.addEventListener("mousemove", function (event) {
+        var boundingRect = box.getBoundingClientRect();
+        var globalY = document.getElementById("globalY");
+        var globalX = document.getElementById("globalX");
+        var localY = document.getElementById("localY");
+        var localX = document.getElementById("localX");
+
+        globalX.innerText = event.clientX;
+        globalY.innerText = event.clientY;
+        localX.innerText = parseInt(event.clientX - boundingRect.left);
+        localY.innerText = parseInt(event.clientY - boundingRect.top);
+    });
+
 });
